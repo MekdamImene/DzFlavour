a236ddbd6f45ff822652aaf3e239524646722837
MekdamImene <mbouchraimene@gmail.com>
2025-05-08 22:36:38 +0100

    Update CheckoutPage.js

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,17 +1,29 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useRef } from 'react';
 import { useCart } from '../context/CartContext';
 import './CheckoutPage.css';
 
 const CheckoutPage = () => {
-  const {cartItems , clearCart } = useCart();
+  const { cartItems, clearCart } = useCart();
+  const hasProcessed = useRef(false);
 
- 
-    
   useEffect(() => {
+    if (hasProcessed.current || cartItems.length === 0) return;
+
+    //  Mark as processed BEFORE running logic
+    hasProcessed.current = true;
     if (cartItems.length > 0) {
-    clearCart();
+      cartItems.forEach(item => {
+        const soldKey = `dish-${item.id}-sold`;
+        const currentSold = parseInt(localStorage.getItem(soldKey)) || 0;
+        const updatedSold = currentSold + item.quantity;
+        localStorage.setItem(soldKey, updatedSold);
+        
+      });
+  
+      clearCart(); // After sold is updated
     }
   }, [cartItems, clearCart]);
+
   return (
     <div className="checkout-success">
       <div className="success-box">
@@ -24,4 +36,5 @@ const CheckoutPage = () => {
 };
 
 export default CheckoutPage;
+
 
